6535fb47c36bc46c1164193a03a14d560656b0ec
BDPeppers <peppersbre@yahoo.com>
2019-01-03 19:49:27 -0500

    arrow fix

---
+++ inc/footer.php
@@ -0,0 +1,73 @@
+
+    <footer>
+        <div class="footer-wrapper">
+            <div class="footer-sect-1">
+                <h1 class="gdrrc">Global Dispute Resolution Resource Center</h1>
+                <ul class="copyright">
+                    <li>@2019 Georgia State Unviersity</li>
+                    <li>College of Law</li>
+                    <li>Consortium on Negotiation and Conflict Resolution</li>
+                </ul>
+                <div class="footer-follow">
+                        <h1 class="footer-sect-header">Follow</h1>
+                        <ul class="footer-follow-list">
+                            <li><a class="footer-link"></a><i class="fab fa-linkedin"></i></li>
+                            <li><a class="footer-link"></a><i class="fab fa-facebook-square"></i></li>
+                            <li><a class="footer-link"></a><i class="fab fa-twitter"></i></li>
+                            <li><a class="footer-link"></a><i class="fas fa-blog"></i></li>
+                        </ul>
+                    </div>
+            </div>
+
+            <div class="footer-sect-2">
+                <div class="footer-explore">
+                    <h1 class="footer-sect-header">Explore</h1>
+
+                    <ul class="footer-list community-list">
+                        <li class="footer-header">Community</li>
+                        <li><a href="#" class="footer-link">Network</a></li>
+                        <li><a href="#" class="footer-link">Opportunities</a></li>
+                        <li><a href="#" class="footer-link">Events</a></li>
+                        <li><a href="#" class="footer-link">Contact Us</a></li>
+                    </ul>
+
+                    <ul class="footer-list adr-list">
+                        <li class="footer-header">ADR Project</li>
+                        <li><a href="#" class="footer-link">About</a></li>
+                        <li><a href="#" class="footer-link">Maps</a></li>
+                        <li><a href="#" class="footer-link">Country List</a></li>
+                        <li><a href="#" class="footer-link">Organizations</a></li>
+                        <li><a href="#" class="footer-link">Submission Form</a></li>
+                    </ul>
+                    <ul class="footer-list education-list">
+                        <li class="footer-header">Educational Resources</li>
+                        <li><a href="#" class="footer-link">Degree Programs</a></li>
+                        <li><a href="#" class="footer-link">Glossary</a></li>
+                        <li><a href="#" class="footer-link">FAQ</a></li>
+                    </ul>
+                    <ul class="footer-list contribute">
+                        <li class="footer-header">Contribute</li>
+                        <li>Submission Forms</li>
+                    </ul>
+                </div3
+
+                
+            </div>
+        </div>
+
+
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, Bootstrap JS, then custom JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <script src="js/index.js" type="application/javascript"></script>
+
+</body>
+
+</html>
